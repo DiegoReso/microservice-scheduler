bcd9c535064dcc3a90deaa762ec90de0918d1b90
diego reis <diegoreso@me.com>
2025-07-28 15:53:03 -0300

    refact: change endpoint to get a user

--- src/main/java/com/reso/scheduler/infrastructure/security/client/UserClient.java
+++ src/main/java/com/reso/scheduler/infrastructure/security/client/UserClient.java
@@ -3,13 +3,14 @@ package com.reso.scheduler.infrastructure.security.client;
 import com.reso.scheduler.business.dto.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "user", url= "${user.url}")
+@FeignClient(name= "users", url= "${user.url}")
 public interface UserClient {
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     UserDTO findUserByEmail(@RequestParam("email") String email,
                             @RequestHeader("Authorization") String token);
 
