f0136768ded24667211bd717f125b749eb8125f6
diego reis <diegoreso@me.com>
2025-07-28 11:25:18 -0300

    feat: create Task repository

---
+++ src/main/java/com/reso/scheduler/infrastructure/repository/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.reso.scheduler.infrastructure.repository;
+
+
+import com.reso.scheduler.infrastructure.entity.TaskEntity;
+import com.reso.scheduler.infrastructure.enums.StatusTaskEnum;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends MongoRepository<TaskEntity, String> {
+
+    List<TaskEntity> findByEventDateBetweenAndStatusTaskEnum(LocalDateTime eventDateAfter,
+                                                             LocalDateTime eventDateBefore,
+                                                             StatusTaskEnum statusTaskEnum);
+
+    List<TaskEntity> findByUserEmail(String email);
+}
