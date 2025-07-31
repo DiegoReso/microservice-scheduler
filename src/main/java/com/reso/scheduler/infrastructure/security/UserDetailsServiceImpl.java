a30092f78956551b8fb1b1213342534b3f14b819
diego reis <diegoreso@me.com>
2025-07-31 17:17:39 -0300

    refact: change injection dependency

--- src/main/java/com/reso/scheduler/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/reso/scheduler/infrastructure/security/UserDetailsServiceImpl.java
@@ -2,16 +2,16 @@ package com.reso.scheduler.infrastructure.security;
 
 import com.reso.scheduler.business.dto.UserDTO;
 import com.reso.scheduler.infrastructure.security.client.UserClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl{
 
-    @Autowired
-    private UserClient client;
+    private final UserClient client;
 
     public UserDetails loadDataUser(String email, String token) {
 
